4f534287e423e0469632ca2c96451d7c3c565f71
Jaqueline Alves Aleixo <jacke.alves1912@gmail.com>
2025-11-17 23:24:02 -0300

    Fechando pagina

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,8 +6,19 @@ import ForgotPassword from "./subScreen/ForgotPassword/ForgotPassword";
 import { IoEarthOutline } from "react-icons/io5";
 import { GrClose } from "react-icons/gr";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function Home() {
+  const [closing, setClosing] = useState(false);
+
+  function handleClose() {
+    setClosing(true);
+
+    setTimeout(() => {
+      window.location.href = "https://google.com";
+    }, 1000); // 1s, se quiser pode aumentar pra 2000
+  }
+
   return (
     <div className="app">
       <aside className="app-aside">
@@ -19,10 +30,21 @@ function Home() {
           <div className="icon-text">
             <IoEarthOutline /> BR
           </div>
-          <div className="icon-text">
+          <div
+            className="icon-text"
+            onClick={handleClose}
+            style={{ cursor: "pointer" }}
+          >
             <GrClose /> Close
           </div>
         </header>
+
+        {/* MENSAGEM DE FECHANDO PÁGINA */}
+        {closing && (
+          <div className="closing-message">
+            Fechando página...
+          </div>
+        )}
 
         <div className="app-content">
           <Routes>
